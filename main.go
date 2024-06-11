abd1309f62d40baf09168d7940c4252a0905e90b
ArturC03 <arturvicentecruz@proton.me>
2024-06-11 21:29:15 +0100

    FINNALY starting screen all set with theme color and correctly centered

--- main.go
+++ main.go
@@ -1,17 +1,18 @@
 package main
 
-import "fmt"
 import (
+    "strings"
+    "fmt"
     "github.com/ArturC03/shellcade/functions"
 )
 
-func main() {
-    
-  width, height, _:= cursor.GetScreenSize()
-  cursor.Clear()
+func StartScreen(width, height int)  {
 
-    asciiArt := `
-  ██████  ██░ ██ ▓█████  ██▓     ██▓     ▄████▄   ▄▄▄      ▓█████▄ ▓█████ 
+  // Set the text color to #03A9F4 (true color)
+  COLOR_CODE:= "\033[38;2;3;169;244m"
+
+  asciiArt := 
+`  ██████  ██░ ██ ▓█████  ██▓     ██▓     ▄████▄   ▄▄▄      ▓█████▄ ▓█████
 ▒██    ▒ ▓██░ ██▒▓█   ▀ ▓██▒    ▓██▒    ▒██▀ ▀█  ▒████▄    ▒██▀ ██▌▓█   ▀ 
 ░ ▓██▄   ▒██▀▀██░▒███   ▒██░    ▒██░    ▒▓█    ▄ ▒██  ▀█▄  ░██   █▌▒███   
   ▒   ██▒░▓█ ░██ ▒▓█  ▄ ▒██░    ▒██░    ▒▓▓▄ ▄██▒░██▄▄▄▄██ ░▓█▄   ▌▒▓█  ▄ 
@@ -21,18 +22,36 @@ func main() {
 ░  ░  ░   ░  ░░ ░   ░     ░ ░     ░ ░   ░          ░   ▒    ░ ░  ░    ░   
       ░   ░  ░  ░   ░  ░    ░  ░    ░  ░░ ░            ░  ░   ░       ░  ░
 `
-// Calculate starting x-coordinate for centering horizontally
-    startX := width/2 - 37
+  // Clear Screen
+  cursor.Clear()
+  // Hide cursor
+  cursor.Hide()
 
-    // Hide cursor
-    cursor.Hide()
+  lines := strings.Split(asciiArt, "\n")
 
+  fmt.Print(COLOR_CODE)
+  
+  // Calculate the starting y-coordinate for centering vertically
+	startY := height/2 - 4
+	
+  for i, line := range lines {
+		// Calculate the x position to center the line
+		startX := width/2 - 74/2
+		cursor.Position(startX, startY+i)
+		fmt.Print(line)
+	}
+  DEFAULT_COLOR := "\033[0m"
+  fmt.Print(DEFAULT_COLOR)
     // Set cursor position and print ASCII art
-    cursor.Position(startX, height/2-4)
-    fmt.Println(asciiArt)
     fmt.Scanln()
     cursor.Clear()
+}
 
+func main() {
     
+  width, height, _:= cursor.GetScreenSize()
+
+  //Show StartScreen
+  StartScreen(width, height)
 }
 
