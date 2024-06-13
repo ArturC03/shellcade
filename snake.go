020065b764907ad553101c47f0e28f4fd2d5f93a
ArturC03 <arturvicentecruz@proton.me>
2024-06-13 01:37:41 +0100

    starting to add games (addition of snake game)

---
+++ snake.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+    "strings"
+    "fmt"
+    "github.com/ArturC03/shellcade/functions"
+    "github.com/eiannone/keyboard"
+    "log"
+)
+
+func RunSnake() {
+  cursor.Clear()
+  width,height,_ := cursor.GetScreenSize()
+  cursor.Position(width/2,height/2)
+
+  if err := keyboard.Open(); err != nil {
+		log.Fatal(err)
+	}
+	defer keyboard.Close()
+
+  for { // Read key
+		char, key, err := keyboard.GetKey()
+		if err != nil {
+			log.Fatal(err)
+		}
+    
+    if char == 's'{
+      
+    }
+
+  }
+
+
+    keyboard.Close()
+}
